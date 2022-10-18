01bbf179de653a0b8a54fae566352c8fa1e3b67a
GopikaAneesh <102350413+GopikaAneesh@users.noreply.github.com>
2022-10-18 15:30:06 +0400

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,25 @@
+package arithmetic;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// Arithmetic operators program
+		short a=30;
+		float f=3.4f;
+		//addition
+		float ans=a+f;
+		System.out.println("sum of "+a+" and "+f+" is "+ans);
+		//subraction
+		ans=a-f;
+		System.out.println("difference of "+a+" and "+f+" is "+ans);
+		//multiplication
+		ans=a*f;
+		System.out.println("product of "+a+" and "+f+" is "+ans);
+		//division
+		ans=a/f;
+		System.out.println("quotient of "+a+" and "+f+" is "+ans);
+		//modulo
+		ans=a%f;
+		System.out.println("remainder of "+a+" and "+f+" is "+ans);
+}
+	}
