a5c13d0d4fc12ea96ab0d4e322f5e772dc0082ac
GopikaAneesh <102350413+GopikaAneesh@users.noreply.github.com>
2022-10-18 14:10:14 +0400

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+package type;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Datatypes
+		int a=10;
+		float f=5.9f;
+		double d=22.45;
+		char c='C';
+		short s=20;
+		byte b=20;
+		long l=3456;
+		boolean bo=true;
+		System.out.println("Integer value is="+a);
+		System.out.println("float value="+f);
+		System.out.println("double value="+d);
+		System.out.println("character value is="+c);
+		System.out.println("short value="+s);
+		System.out.println("byte value ="+b);
+		System.out.println("long value="+l);
+		System.out.println("boolean value="+bo);
+	}
+
+}
