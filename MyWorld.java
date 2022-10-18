8e873deae5713d6c36a503b2e4f6373705edd181
GopikaAneesh <102350413+GopikaAneesh@users.noreply.github.com>
2022-10-18 13:15:42 +0400

    Add files via upload

---
+++ MyWorld.java
@@ -0,0 +1,13 @@
+package MyProject;
+
+public class MyWorld {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Gopika");
+		System.out.println("I am studying in edubridge");
+		System.out.println("My batch number is 8108");
+	
+
+	}
+
+}
