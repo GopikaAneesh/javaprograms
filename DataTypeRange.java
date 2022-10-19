7133e1d840084c8623667f25aa71e29d0ef542aa
GopikaAneesh <102350413+GopikaAneesh@users.noreply.github.com>
2022-10-19 19:22:18 +0400

    Add files via upload

---
+++ DataTypeRange.java
@@ -0,0 +1,43 @@
+package range;
+
+public class DataTypeRange {
+
+	public static void main(String[] args) {
+		// Range of data types
+		//boolean
+		boolean b=true;
+		System.out.println(b);
+		
+		//byte
+		byte b1=127;
+		System.out.println(b1);
+		
+		//short
+		short s=-129;
+		System.out.println(s);
+		
+		//integer
+		int i=300000000;
+		System.out.println(i);
+		
+		//long
+		long l=30000000000L;
+		System.out.println(l);
+		
+		//double
+		double d=99.99;
+		System.out.println(d);
+		
+		//floating-point
+		float f=9.99f;
+		System.out.println(f);
+		
+		//character
+		char c='\u0041';
+		System.out.println(c);
+		char c1='A';
+		System.out.println(c1);
+		
+	}
+
+}
